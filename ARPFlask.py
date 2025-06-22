a1f9364b9fdebbe50e18c28aa59aed095bb71fde
Garrett <Garrettcook115@gmail.com>
2025-06-22 10:43:35 -0400

    Edits for linux operability and execution fixes

--- ARPFlask.py
+++ ARPFlask.py
@@ -2,15 +2,23 @@ from flask import Flask, render_template, request
 import subprocess
 import sys
 
-    
-# Ensure Windows execution
-if sys.platform != "win32":
-    print("This application only functions on a Windows System")
-    sys.exit()
-
 app = Flask(__name__)
 
 # Command Execution
+if sys.platform =="win32":
+    command_map={
+        "ipconfig": "ipconfig ",
+        "arp": "arp -a",
+        "nslookup": "nslookup google.com"
+    }
+else:
+    command_map = {
+        "ifconfig": "ifconfig",
+        "arp": "arp -a",
+        "nslookup": "nslookup google.com"
+        
+    }
+
 def execute_command(cmd):
     result = subprocess.run(cmd, shell=True, capture_output=True, text=True)
     return result.stdout
@@ -21,25 +29,17 @@ def index():
 
 @app.route("/run_command", methods=["POST"])
 def run_command():
-    command = request.form["command"]
-    
-    command_map = {
-        "ipconfig": "ipconfig /all",
-        "arp": "arp -a",
-        "nslookup": "nslookup google.com"
-        
-    }
-
-    cmd = command_map.get(command, None)
+    command = request.form["command"].lower()
+    cmd = command_map.get(command)
     if not cmd:
         return "Invalid command selected!"
     
     output = execute_command(cmd)
     return f"<pre>{output}</pre>"
 
-if __name__ == "__main__":
-    app.run(debug=True)
-
-if __name__ == "__main__":
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
+if __name__=="__main__":
+    if sys.platform =="win32":
+        app.run(debug=True)
+    else:
+        from waitress import serve
+        serve(app, host="0.0.0.0", port=8080)
